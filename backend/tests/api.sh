14731ab3cc3460f40dc2da6660616a03cd38fe58
Rohan Gautam <rohan1gautam@gmail.com>
2023-08-20 17:07:45 +0800

    basic api testing, incomplete

---
+++ backend/tests/api.sh
@@ -0,0 +1,13 @@
+## very bare bones, just to see if things work
+
+base_url="http://127.0.0.1:8000"
+# echo "Testing Latest news researcher agent @ /latest_news "
+# curl -X GET "${base_url}/latest_news/india"
+# curl -X GET "${base_url}/latest_news/singapore"
+# curl -X GET "${base_url}/latest_news/usa"
+
+
+echo "Testing country Q&A @ /qa "
+query = "q=What should i keep in mind about sustainability regulations as a tech company"
+# echo str_replace(' ','%20',$query);
+# curl -X GET "${base_url}/qa/usa" -d 
