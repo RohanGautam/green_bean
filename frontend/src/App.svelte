206388a7ef8a2afe1c99be50bd185d79ae17a342
Rohan Gautam <rohan1gautam@gmail.com>
2023-08-20 18:58:13 +0800

    making fetch request to api

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -6,6 +6,43 @@
   let user_country: string = "Singapore";
   let user_business_name: string = "";
   let user_industry: string = "";
+  // TODO : make tweakable environment variable for easier deployment?
+  const API_BASE_URL = "http://127.0.0.1:8000";
+
+  const country_namespace_map = {
+    Singapore: "singapore",
+    India: "india",
+    USA: "usa",
+  };
+
+  let show_loading = false;
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  function get_latest_news() {
+    // no form validation, bless our soul
+    let namespace = country_namespace_map[user_country];
+    const url = `${API_BASE_URL}/latest_news/${namespace}`;
+    // const url = `${API_BASE_URL}`;
+    show_loading = true;
+    console.log(url);
+    // for testing
+    sleep(2000).then(() => {
+      show_loading = false;
+    });
+    // fetch(url)
+    //   .then((response) => response.json())
+    //   .then((v) => {
+    //     console.log(v);
+    //     show_loading = false;
+    //   })
+    //   .catch((err) => {
+    //     console.log("error encountered");
+    //     console.log(err);
+    //     show_loading = false;
+    //     return [];
+    //   });
+  }
 </script>
 
 <main>
@@ -18,7 +55,7 @@
     <div class="navbar-end text-primary-content" />
   </div>
 
-  <div class=" bg-base-200 p-8 m-2 rounded-md">
+  <div class="bg-base-200 p-8 m-2 rounded-md">
     <p class="text-xl">Let's get to know you!</p>
     <div class="flex flex-row justify-evenly">
       <div class="form-control w-full max-w-xs">
@@ -57,6 +94,27 @@
         />
       </div>
     </div>
+  </div>
+
+  <div class="bg-base-200 p-8 m-2 rounded-md">
+    <p class="text-xl">
+      Get to know the latest sustainability regulation news summary for {user_country}
+    </p>
+    <p class="text-md">
+      Specially crafted by an autonomous agent for you 🤖❤️ (this might take a
+      around 30-40s)
+    </p>
+    <button
+      class="btn btn-outline"
+      on:click={() => {
+        get_latest_news();
+      }}
+    >
+      Generate!
+      {#if show_loading}
+        <span class="loading loading-spinner" />
+      {/if}
+    </button>
   </div>
   <!-- <p>{user_country}, {user_business_name}, {user_industry}</p> -->
 
